5f1c9754bfdd434eabf0feba35eef144cb83a192
dntf <*****@users.noreply.github.com>
2021-08-27 16:35:51 +0900

    v0.2

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 function eng(){
-    let str = ""
-    str += "<h2>Normal Counter - Privacy policy</h2>"
-    str += '<strong>"Using Ads ID"</strong>'
-    str += "<p>This app uses he unique id to display ads.</p>"
-    str += "<p>this id is used to sent data.</p>"
-    str += "<p>Your personal information will not be identification from this id. </p>"
-    str += '<strong>"Network access"</strong>'
-    str += "<p>That used to display ads. </p>"
-    document.getElementById('lang_1').innerHTML = str
+    loadHtml("./src/index_en.html","container");
 }
 
 function jpn(){
-    let str = ""
-    str += "<h2>ノーマルカウンター - プライバシーポリシー</h2>"
-    str += '<strong>『広告IDの利用』</strong>'
-    str += "<p>広告を表示する際に固有のIDを利用しています。</p>"
-    str += "<p>これはアプリから送信され利用されます。</p>"
-    str += "<p>このIDから個人が特定されることはありません。</p>"
-    str += '<strong>『ネットワークアクセス』</strong>'
-    str += "<p>広告を表示するために行われます。</p>"
-    document.getElementById('lang_1').innerHTML = str
+    loadHtml("./src/index_ja.html","container");
+}
+
+function loadHtml(html_,place){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET",html_,true);
+    xmlhttp.onreadystatechange = function(){
+        if(xmlhttp.readyState==4&&xmlhttp.status==200){
+            var data = xmlhttp.responseText;
+            var element = document.getElementById(place);
+            element.innerHTML = data;
+        }
+        return data;
+    }
+    xmlhttp.send(null);
 }
